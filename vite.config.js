344ff36d83ced8a865eb54ef65c62971060a0db9
Deepak Patil <harshzopem21@gmail.com>
2025-12-29 22:15:38 +0530

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://hersheys6969.github.io/Focus-Lock_TechSprint_GDG/",
 })
